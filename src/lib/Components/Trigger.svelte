6a82d1e28ad9859ff35619b291c7e394c416942a
mishba-ai <mishbabagban@gmail.com>
2024-07-26 19:03:00 +0530

    add the supabase google auth

--- src/lib/Components/Trigger.svelte
+++ src/lib/Components/Trigger.svelte
@@ -4,12 +4,12 @@
     const modalId = getContext('modalId');
     // getcontext is used to access the value that was set with setcontext. You call this function in any component that wants to use the data provided by the context.
 </script>
-<span on:click={() => $id = modalId}>
+<button on:click={() => $id = modalId}>
 	<slot />
-</span>
+</button>
 
 <style>
-	span:hover	 {
+	button:hover	 {
 		cursor: pointer;
 	}
 </style>
---
+++ src/routes/auth/login/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { supabase } from "../../../lib/helper/supabaseClient.js";
+   import {page} from '$app/stores';
+
+    
+        const login = async() => await  supabase.auth.signInWithOAuth({
+          provider: "google",
+        });
+    
+
+</script>
+
+<body class="w-screen">
+  <button
+    on:click={login}
+    class="border w-32 flex justify-center items-center hover:bg-slate-400 rounded-2xl bg-slate-400"
+    >Login with google</button
+  >
+</body>
